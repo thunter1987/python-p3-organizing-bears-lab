f24f5da7539e21d0b383d832d0677541552275f9
Tony Hunter <thunter1987@yahoo.com>
2023-08-06 18:36:09 -0500

    Passes Tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, 'Mr. Chocolate', 26, 'M', 'Brown', NULL, 1),
+(2, 'Rowdy', 10, 'M', 'Black', NULL, 1),
+(3, 'Tabitha', 8, 'F', 'Brown', NULL, 1),
+(4, 'Sergeant Brown', 44, 'M', 'Brown', NULL, 0),
+(5, 'Melissa', 4, 'F', 'Black', NULL, 1),
+(6, 'Grinch', 98, 'M', 'Black', NULL, 1),
+(7, 'Wendy', 29, 'F', 'Brown', NULL, 1),
+(8, NULL, 30, 'M', 'Black', NULL, 1);
